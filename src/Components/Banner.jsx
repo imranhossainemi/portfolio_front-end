f44b501ad522088a3dc402c91fc4199ac99fdea6
Imran Hossain <blur-imran@hotmail.com>
2023-08-08 12:48:15 +0600

    Create Banner.jsx

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Banner = () => (
+  <section className="banner">
+    <div className="headline">
+      <h1>
+        Hey there. I am Imran Hossain
+        {' '}
+        <span>I am a software developer</span>
+        {' '}
+      </h1>
+    </div>
+    <div className="article">
+      <p>
+        I can help you build a product ,
+        feature or website Look through
+        some of my work and experience!
+        If you like what you see
+        and have a project you need coded,
+        do not hestiate to contact me.
+      </p>
+    </div>
+  </section>
+);
+
+export default Banner;
