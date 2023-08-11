b0f41c3291708b684f11a1dcfd1531cc5cb61c90
Imran Hossain <blur-imran@hotmail.com>
2023-08-11 18:14:48 +0600

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
+import styles from '../Styles/Header.module.css';
 
 const Header = () => (
-  <header>
-    <div className="navSec">
-      <div className="logo">
+  <header className={styles.header}>
+    <div className={styles.navSec}>
+      <div className={styles.logo}>
         <p>welcome</p>
       </div>
       <Navbar />
