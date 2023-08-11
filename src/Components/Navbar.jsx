831fb47c27650a439523aadcbc215ead22830969
Imran Hossain <blur-imran@hotmail.com>
2023-08-11 17:04:50 +0600

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,17 @@
-import React, { Link } from 'react';
+import React from 'react';
 
 const Navbar = () => (
   <nav>
     <button className="mobile-nav-btm" id="open" type="button">  </button>
     <ul className="navigation">
       <li className="nav-item">
-        <Link to="#portfolio" className="nav-link">Portfolio</Link>
+        <a href="/portfolio" className="nav-link">Portfolio</a>
       </li>
       <li className="nav-item">
-        <a href="#about" className="nav-link">About</a>
+        <a href="/about" className="nav-link">About</a>
       </li>
       <li className="nav-item">
-        <a href="#contact" className="nav-link">Contact</a>
+        <a href="/contact" className="nav-link">Contact</a>
       </li>
     </ul>
   </nav>
