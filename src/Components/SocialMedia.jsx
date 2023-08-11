a3fa1e8d43a89dee45b094a263105e230d4215d9
Imran Hossain <blur-imran@hotmail.com>
2023-08-11 18:14:54 +0600

    Update SocialMedia.jsx

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -3,9 +3,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faGithub, faLinkedinIn, faTwitter, faAngellist, faMedium,
 } from '@fortawesome/free-brands-svg-icons';
+import styles from '../Styles/SocialMedia.module.css';
 
 const SocialMedia = () => (
-  <div className="socialMedia">
+  <div className={styles.socialMedia}>
     <ul>
       <li>
         <a href="/#" target="_blank" rel="noopener" aria-label="github">
