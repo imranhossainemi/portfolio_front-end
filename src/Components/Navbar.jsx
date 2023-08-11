178202a38b21294118cec70fbc2dec121c8f09bd
Imran Hossain <blur-imran@hotmail.com>
2023-08-11 18:14:50 +0600

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import styles from '../Styles/Navbar.module.css';
 
 const Navbar = () => (
   <nav>
-    <button className="mobile-nav-btm" id="open" type="button">  </button>
-    <ul className="navigation">
-      <li className="nav-item">
-        <a href="/portfolio" className="nav-link">Portfolio</a>
+    <button className={styles.mobileNavBtm} id="open" type="button">  </button>
+    <ul className={styles.navigation}>
+      <li className={styles.navItem}>
+        <a href="#portfolio" className={styles.navLink}>Portfolio</a>
       </li>
-      <li className="nav-item">
-        <a href="/about" className="nav-link">About</a>
+      <li className={styles.navItem}>
+        <a href="#about" className={styles.navLink}>About</a>
       </li>
-      <li className="nav-item">
-        <a href="/contact" className="nav-link">Contact</a>
+      <li className={styles.navItem}>
+        <a href="#contact" className={styles.navLink}>Contact</a>
       </li>
     </ul>
   </nav>
