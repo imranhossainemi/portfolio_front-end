d4adbcd4dbf92296526bacadd6c2ee778a043787
Imran Hossain <blur-imran@hotmail.com>
2023-08-13 01:10:09 +0600

    Create About.jsx

---
+++ src/Components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Myself from './Myself';
+
+const About = () => (
+  <section className="about sec" id="about">
+    <Myself />
+  </section>
+);
+
+export default About;
